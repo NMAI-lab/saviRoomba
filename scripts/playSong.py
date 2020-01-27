c128fc71389c79f2b2f1c446a9c6ac19889c4844
Patrick Gavigan <pgavigan@gmail.com>
2020-01-27 14:44:24 -0500

    Added song player

---
+++ scripts/playSong.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 14:38:44 2020
+
+@author: patrickgavigan
+"""
+
+
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('play_song', String, queue_size=10)
+    rospy.init_node('songPlayer', anonymous=True)
+    rate = rospy.Rate(1) # 1hz
+    while not rospy.is_shutdown():
+        message = 1
+        rospy.loginfo(message)
+        pub.publish(message)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
