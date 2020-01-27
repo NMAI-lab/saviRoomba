ccb36fa46fccd678722d352f03ec16ff5671a60c
Patrick Gavigan <pgavigan@gmail.com>
2020-01-27 15:32:19 -0500

    removed loop from playSong

--- scripts/playSong.py
+++ scripts/playSong.py
@@ -4,20 +4,18 @@ Created on Mon Jan 27 14:38:44 2020
 
 @author: patrickgavigan
 """
-
-
 import rospy
 from std_msgs.msg import String
 
 def playSong():
     pub = rospy.Publisher('play_song', String, queue_size=10)
     rospy.init_node('songPlayer', anonymous=True)
-    rate = rospy.Rate(1) # 1hz
-    while not rospy.is_shutdown():
-        message = 1
-        rospy.loginfo(message)
-        pub.publish(message)
-        rate.sleep()
+    #rate = rospy.Rate(1) # 1hz
+    #while not rospy.is_shutdown():
+    message = 1
+    rospy.loginfo(message)
+    pub.publish(message)
+    #rate.sleep()
 
 if __name__ == '__main__':
     try:
