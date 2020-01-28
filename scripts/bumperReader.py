be748d86bee3064c599c7cc820f2e6a74adbe5d6
Patrick Gavigan <pgavigan@gmail.com>
2020-01-28 16:40:30 -0500

    First attempt at reading bumper messages

---
+++ scripts/bumperReader.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# Created on Mon Jan 27 14:38:44 2020
+# @author: patrickgavigan
+
+import rospy
+from ca_msgs.msg import Bumper
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    #rospy.init_node('actionListener', anonymous=True)
+    rospy.init_node('bumperListener', anonymous=True)
+    
+    rospy.Subscriber('bumper', Bumper, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
