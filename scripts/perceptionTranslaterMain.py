a81783625981481ebb76f07dfb984b662a17f1ee
Patrick Gavigan <pgavigan@gmail.com>
2020-02-04 15:11:28 -0500

    Translating bumper messages as perceptions

---
+++ scripts/perceptionTranslaterMain.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+#Created on Tue Feb  4 14:52:12 2020
+#
+#@author: patrickgavigan
+
+import rospy
+from std_msgs.msg import String
+from ca_msgs.msg import Bumper
+
+
+# Receive the bumper message and call the publisher
+def receiveSensorData(sensorData):
+    perceptionMessage = "bumper(" + str(sensorData.is_left_pressed) + "," + str(sensorData.is_right_pressed) + ")"
+    publishAsPerception(perceptionMessage)
+
+
+# Publish the bumper perception
+def publishAsPerception(message):
+    pub = rospy.Publisher('perceptions', String, queue_size=10)
+    rospy.loginfo(message)
+    pub.publish(message)
+
+
+# Main method: subscribe to relevant sensor data type and receive the data
+def perceptionTranslatorMain():
+
+    # Setup the node
+    rospy.init_node('perceptionTranslater', anonymous=True)
+    
+    # Listen to bumper messages
+    rospy.Subscriber('bumper', Bumper, receiveSensorData)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+    
+
+if __name__ == '__main__':
+    try:
+        perceptionTranslatorMain()
+    except rospy.ROSInterruptException:
+        pass
