c5db9e6f3ed93d296903533d07a21f8e5a9afd2e
Patrick Gavigan <pgavigan@gmail.com>
2020-09-03 23:25:41 -0400

    Fixed issue with liner.py
    Check GPIO only once per cycle

--- scripts/liner.py
+++ scripts/liner.py
@@ -17,20 +17,24 @@ GPIO.setup(left_sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 def get_line():
-    if GPIO.input(center_sensor) == 1 and GPIO.input(right_sensor) == 0 and GPIO.input(left_sensor) == 0:
-        return "c"
-    elif GPIO.input(right_sensor) == 1 and GPIO.input(center_sensor) == 0 and GPIO.input(left_sensor) == 0:
-        return "r"
-    elif GPIO.input(right_sensor) == 1 and GPIO.input(center_sensor) == 1 and GPIO.input(left_sensor) == 0:
-        return "r"
-    elif GPIO.input(left_sensor) == 1 and GPIO.input(right_sensor) == 0 and GPIO.input(center_sensor) == 0:
-        return "l"
-    elif GPIO.input(left_sensor) == 1 and GPIO.input(right_sensor) == 0 and GPIO.input(center_sensor) == 1:
-        return "l"
-    elif GPIO.input(center_sensor) == 1 and GPIO.input(right_sensor) == 1 and GPIO.input(left_sensor) == 1:
-        return "a"
+    centerLED = GPIO.input(center_sensor)
+    leftLED = GPIO.input(left_sensor)
+    rightLED = GPIO.input(right_sensor)
+    LEDs = (leftLED, centerLED, rightLED)
+    
+    # 0 indicates the location of the line
+    if LEDs == (1,0,1):
+        result = "c"
+    elif (LEDs == (1,1,0)) or (LEDs == (1,0,0)):
+        result = "r"
+    elif (LEDs == (0,1,1)) or (LEDs == (0,0,1)):
+        result = "l"
+    elif LEDs == (0,0,0):
+        result = "a"
     else:
-        return "ll"
+        result = "ll"
+        
+    return (result, LEDs)
     
 
 def liner():
@@ -40,17 +44,19 @@ def liner():
 
     while not rospy.is_shutdown():
 
-        if get_line() == "c":
+        (linePosition, LEDs) = get_line()        
+
+        if linePosition == "c":
             line = "line(center)"
-        elif get_line() == "r":
+        elif linePosition == "r":
             line = "line(right)"
-        elif get_line() == "l":
+        elif linePosition == "l":
             line = "line(left)"
-        elif get_line() == "a":
+        elif linePosition == "a":
             line = "line(across)"
         else:
             line = "line(lost)"
-        sense = "{}, {}, {}".format(GPIO.input(left_sensor), GPIO.input(center_sensor), GPIO.input(right_sensor))
+        sense = "{}, {}, {}".format(LEDs[0], LEDs[1], LEDs[2])
         rospy.loginfo(sense)
         pub.publish(line)
         rate.sleep()
