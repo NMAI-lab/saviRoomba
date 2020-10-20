5347b928598b0b7519de956f22cfa5335004e1fb
Patrick Gavigan <pgavigan@gmail.com>
2020-10-19 22:12:47 -0400

    Ignore actions movement actions when busy with other movement actions

--- scripts/actionTranslator.py
+++ scripts/actionTranslator.py
@@ -8,10 +8,12 @@ from geometry_msgs.msg import Twist
 from driverLineSensor import getLine
 
 lastTurn = "left"
+busy = False
+
 
 # Decode and execute the action
 def decodeAction(data, args):
-    
+        
     # Get the parameters
     action = str(data.data)
     (drivePublisher, dockPublisher, undockPublisher, destinationPublisher) = args
@@ -23,34 +25,43 @@ def decodeAction(data, args):
     elif action == "station(undock)":
         undockPublisher.publish()   # Publish to the undock topic
         
-        for i in range(10):
-            drive(drivePublisher,"back")
+#        for i in range(10):
+#            drive(drivePublisher,"back")
         
         turn(drivePublisher,"left") # Turn the robot around
         
     # Extract the action parameter between the brackets
     parameter = re.search('\((.*)\)', action).group(1)
-    
-    # Deal with drive action
-    if re.search("drive", action):
-        drive(drivePublisher,parameter)
-        
-    # Deal with turn message (similar to drive action but continues until the 
-    # line sensor detects "c")
-    elif re.search("turn", action):
-        turn(drivePublisher,parameter)
-    
+
     # Deal with passing setDestination action to the appropriate topic
-    elif re.search("setDestination", action):
+    if re.search("setDestination", action):
         destinationPublisher.publish(parameter)
+
+    global busy
+    if not busy:
+        busy = True
     
-    # Deal with invalid action
-    else:
-        rospy.loginfo("Invalid action ignored")
+        # Deal with drive action
+        if re.search("drive", action):
+            drive(drivePublisher,parameter)
+        
+        # Deal with turn message (similar to drive action but continues until the 
+        # line sensor detects "c")
+        elif re.search("turn", action):
+            turn(drivePublisher,parameter)
+    
+        # Deal with passing setDestination action to the appropriate topic
+        #elif re.search("setDestination", action):
+        #    destinationPublisher.publish(parameter)
+    
+        # Deal with invalid action
+        else:
+            rospy.loginfo("Invalid action ignored")
+            
+        busy = False
 
 # Turn command, repeated drive commands until the lince sensor detects c again
 def turn(publisher, parameter):
-    
     # Get the turn started
     drive(publisher,parameter)
     
@@ -108,14 +119,29 @@ def getTwistMesg(parameter, drive):
     
     return message
 
+#def publishTurn(pub):
+#    rate = rospy.Rate(10)
+#    
+#    while not rospy.is_shutdown():
+#        global turning
+#        if turning:
+#            message = "busy(turning)"
+#            rospy.loginfo(message)
+#            pub.publish(message)
+#        rate.sleep()
+   
+
 # Main execution
 def rosMain():
     drivePublisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     dockPublisher = rospy.Publisher('dock', Empty, queue_size=10)
     undockPublisher = rospy.Publisher('undock', Empty, queue_size=10)
     destinationPublisher = rospy.Publisher('setDestination', String, queue_size=10)
+    #perceptionsPublisher = rospy.Publisher('perceptions', String, queue_size=10)
     rospy.init_node('actionTranslator', anonymous=True)
     rospy.Subscriber('actions', String, decodeAction, (drivePublisher, dockPublisher, undockPublisher, destinationPublisher))
+    
+    #publishTurn(perceptionsPublisher)
     rospy.spin()
 
 # Start things up
