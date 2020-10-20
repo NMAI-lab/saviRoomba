682d0e0905b618a38cca031bdfae0fdf622d4f57
Patrick Gavigan <pgavigan@gmail.com>
2020-10-20 10:12:22 -0400

    adjusted turn

--- scripts/actionTranslator.py
+++ scripts/actionTranslator.py
@@ -75,7 +75,8 @@ def turn(publisher, parameter):
     
     # Keep turning until the line is centered again
     i = 0
-    while (getLine()[0] != "c") or (i < 500):
+    while (i < 500):
+    #while (getLine()[0] != "c") or (i < 500):
         drive(publisher,parameter, False)
         i += 1
             
