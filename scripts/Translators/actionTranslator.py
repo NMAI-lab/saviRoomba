08da8a206e0f61e566b640f1c33edc0d6c4a240a
Simon <simon.c.yacob@gmail.com>
2021-06-30 15:47:32 -0400

    Updated Action Translator to comply with format and added drivexy(x,y)
    Action tranlslator now uses action(parameter) format
    and I have added drivexy(x,y) action for when you want to give specific x and y inputs instead of generic inputs like "drive(forward"

--- scripts/Translators/actionTranslator.py
+++ scripts/Translators/actionTranslator.py
@@ -25,28 +25,83 @@ def decodeAction(data, args):
     action = str(data.data)
     (drivePublisher, dockPublisher, undockPublisher) = args
     rospy.loginfo("Action: " + action)
-    
-    #handle basic movement commands from actions topic
+
+    """
+    Handle basic movement commands from actions topic
+    Actions:
+    drivexy(x,y) Drive the robot linearly with x(+ forward, - backward) and angularly with y(+ right, - left)
+    drive(forward) Drive the robot forward
+    drive(stop) Stops the robot
+    drive(backward) Drive the robot backward
+    drive(sleft) Drive the robot slightly left
+    drive(sright) Drive the robot slightly right
+    drive(bleft) Drives the robot back and a little left. This is used when the robot gets to close to the wall and needs to realign itself
+    drive(avoidright) Drives in an arc to avoid an obstacle to the robots right.
+    turn(left) Does a 45 degree turn left (stops robot first)
+    turn(right) Does 45 degree turn right (stops robot first)
+    station(dock) Docks the robot
+    station(undock) Undocks the robot
+    """
     actionMessage = getTwistMesg(action)
-    if(action == "left"): #Does a 45 degree turn left (stops robot first)
-        actionMessage = getTwistMesg("left")
-        tmp = String()
-        tmp.data = "stop"
-        decodeAction(tmp, args)
-        drivePublisher.publish(actionMessage)
-    elif(action == "right"): #Does 45 degree turn right (stops robot first)
-        actionMessage = getTwistMesg("right")
-        tmp = String()
-        tmp.data = "stop"
-        decodeAction(tmp, args)
-        drivePublisher.publish(actionMessage)
-    elif (action == "dock"):
+
+    if re.search("drivexy", action): #if it is a drivexy action
+        parameter = re.search('\((.*)\)', action).group(1) # Extract the "x,y" between the brackets 
+        parameter = parameter.split(",") #split around the comma
+        x = float(parameter[0]) #convert x value into float from string
+        y = float(parameter[1]) #convert y value into float from string
+        #perform safety/sanity checks on the drive values
+        if(abs(x) <= 0.5 and abs(y) <= 4.25):
+            message = Twist()
+            message.linear.x = x
+            message.angular.z = y
+            drivePublisher.publish(message)
+     
+    elif re.search("drive", action): #if it is a drive action
+        if(action == "drive(forward)"): # Drive the robot forward
+            actionMessage = getTwistMesg("forward")
+            drivePublisher.publish(actionMessage)
+        elif(action == "drive(stop)"): # Stops the robot
+            actionMessage = getTwistMesg("drive(stop)")
+            drivePublisher.publish(actionMessage)
+        elif(action == "drive(backward)"): # Drive the robot backward
+            actionMessage = getTwistMesg("backward")
+            drivePublisher.publish(actionMessage)
+        elif(action == "drive(sleft)"): # Drive the robot backward
+            actionMessage = getTwistMesg("sleft")
+            drivePublisher.publish(actionMessage)
+        elif(action == "drive(sright)"): # Drive the robot backward
+            actionMessage = getTwistMesg("sright")
+            drivePublisher.publish(actionMessage)
+        elif(action == "drive(bleft)"): # Drive the robot backward
+            actionMessage = getTwistMesg("bleft")
+            drivePublisher.publish(actionMessage)
+        elif(action == "drive(avoidright)"): # Drive the robot backward
+            actionMessage = getTwistMesg("avoidright")
+            drivePublisher.publish(actionMessage)
+            
+    elif re.search("turn", action): #if it is a turn action
+        if(action == "turn(left)"): #Does a 45 degree turn left (stops robot first)
+            actionMessage = getTwistMesg("left")
+            tmp = String()
+            tmp.data = "stop"
+            decodeAction(tmp, args)
+            drivePublisher.publish(actionMessage)
+        elif(action == "turn(right)"): #Does 45 degree turn right (stops robot first)
+            actionMessage = getTwistMesg("right")
+            tmp = String()
+            tmp.data = "drive(stop)"
+            decodeAction(tmp, args)
+            drivePublisher.publish(actionMessage)
+        
+    #If it is a station action
+    elif action == "station(dock)":
         dockPublisher.publish()
-    elif(action == 'undock'):
-        undockPublisher.publish()
-    else:
-        #publish action
-        drivePublisher.publish(actionMessage)
+    elif action == "station(undock)":
+        undockPublisher.publish()   # Publish to the undock topic
+    
+    
+
+    
         
 """
 Get a Twist message which consists of a linear and angular component which can be negative or positive.
@@ -72,10 +127,10 @@ def getTwistMesg(action):
     elif action == "right":
         message.linear.x = 0
         message.angular.z = -4
-    elif action == "sleft": # Sudden/Sharp left (not sure need to test)
+    elif action == "sleft": # slight left (not sure need to test)
         message.linear.x = 0.05
         message.angular.z = 0.5
-    elif action == "sright": # Sudden/Sharp right (not sure need to test)
+    elif action == "sright": # slight right (not sure need to test)
         message.linear.x = 0.05
         message.angular.z = -0.5
     elif action == "avoidright": #obstacle avoidance when obstacle is to the right of the object
