4f95a39a5de3aa4ae9ce42f1a5ce64e1995136d1
Patrick Gavigan <pgavigan@gmail.com>
2021-07-06 09:42:02 -0400

    first round of the node

--- scripts/sensors/BeaconNode.py
+++ scripts/sensors/BeaconNode.py
@@ -5,3 +5,33 @@
 
 # SUBSCRIBER:   none
 # PUBLISHER:    String object to 'beacons' node
+
+import rospy
+import time
+from bluepy.btle import Scanner
+from std_msgs.msg import String
+from reader import BeaconReader
+
+
+def runBeacons(pub):
+    print("hello beacons")
+
+
+def rosMain():
+    
+    pub = rospy.Publisher('/sensors/Beacon', String, queue_size=5)
+    rospy.init_node('bluetoothBeacons', anonymous=True)
+    rate = rospy.Rate(60)
+    
+    while not rospy.is_shutdown():
+        runBeacons(pub)
+        rate.sleep()
+    
+
+
+if __name__ == '__main__':
+    try:
+        rosMain()
+    except rospy.ROSInterruptException:
+        pass
+
