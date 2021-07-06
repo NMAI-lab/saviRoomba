772590b3d7fe7df2622ab1c7a1fae99ec21caa29
Patrick Gavigan <pgavigan@gmail.com>
2021-07-06 11:33:27 -0400

    beaconNode ready for testing

--- scripts/sensors/BeaconNode.py
+++ scripts/sensors/BeaconNode.py
@@ -7,10 +7,10 @@
 # PUBLISHER:    String object to 'beacons' node
 
 import rospy
-import time
 from bluepy.btle import Scanner
 from std_msgs.msg import String
 from reader import BeaconReader
+import statistics
 
 
 # Polls for bluetooth devices, returns signal strength in a dict, indexed by
@@ -38,32 +38,73 @@ def calculateDistance(beaconData,beaconParameters):
         distances[mac] = pow(10,(measuredValue - beaconData[mac])/(10*environmentVariable))
     return distances
     
-    
+def manageBeaconQueue(beaconHistory,newData,queueLength=10):
+    beaconNames = beaconHistory.keys()
+    newBeaconDataNames = newData.keys()
 
+    for currentBeacon in beaconNames:
+        currentQueue = beaconHistory[currentBeacon]
+
+        if currentBeacon in newBeaconDataNames:
+            currentQueue.append(newData[currentBeacon])
+        
+        currentQueue = removeOutliers(currentQueue)
+        
+        # If the queue is too long, pop the oldest element
+        if len(currentQueue) > queueLength:
+            currentQueue.pop(0)
+        
+    return beaconHistory
+
+    
+# Remove outliers from range list
 def removeOutliers(rangeList):
-    # Remove outliers from range list
-    print("outlier function")
+    dataMean = statistics.mean(rangeList)
+    dataStd = statistics.stdev(rangeList)
+    maxValue = dataMean + (3 * dataStd)
+    minValue = dataMean - (3 * dataStd)
     
+    for i in range(len(rangeList)):
+        if (rangeList[i] < minValue) or (rangeList[i] > maxValue):
+            rangeList[i] = -1       # Flag the outlier
+            
+    rangeList = filter(lambda a: a != -1, rangeList) 
+    
+    return rangeList
+
+    
+# Get averages for the distances
+def calculateAverages(beaconData):
+    beaconNames = beaconData.keys()
+    beaconAverages = dict()
+    for beaconName in beaconNames:
+        beaconAverages[beaconName] = statistics.mean(beaconData[beaconName])
+    return beaconAverages
+
+
 def publishData(pub, data):
+    message = ""
+    beaconNames = data.keys()
+    for beaconName in beaconNames:
+        message = message + "beacon(" + str(data[beaconName]) + ")"
     
-    # Publish the data to ROS
-    print("publish data")
+    rospy.loginfo(message)
+    pub.publish(message)
         
 
-def runBeacons(pub,rate,beaconParameters):
-    #print("hello beacons")
-    
+def runBeacons(pub,rate,beaconParameters,beaconHistory):
     period = 1.0/rate
     beaconData = pollBeacons(beaconParameters, period)
     distances = calculateDistance(beaconData,beaconParameters)
-    print(beaconData)
-    print(distances)
-    
-    
-    #print("goodbye beacons")
-    
-    
 
+    beaconHistory = manageBeaconQueue(beaconHistory,distances)
+
+    # Calculate the average for each queue
+    beaconAverage = calculateAverages(beaconHistory)
+    
+    # Publish averages
+    publishData(pub,beaconAverage)
+    
 
 def rosMain():
     
@@ -77,9 +118,15 @@ def rosMain():
     beaconReader = BeaconReader()
     beaconParameters = beaconReader.read_beacons()
     
+    # Dict for the beacon history data queues
+    beaconHistory = dict()
+    beaconNames = beaconParameters.keys()
+    for beacon in beaconNames:
+        beaconHistory[beacon] = list()
+    
     # Run the node
     while not rospy.is_shutdown():
-        runBeacons(pub,frequency,beaconParameters)
+        runBeacons(pub,frequency,beaconParameters,beaconHistory)
         rate.sleep()
     
 
