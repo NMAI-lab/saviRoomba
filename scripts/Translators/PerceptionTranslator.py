5e19d1f1f746943d778abd49f357d16f7060aa3b
Patrick Gavigan <pgavigan@gmail.com>
2021-07-08 14:30:07 -0400

    Updated message type for odom

--- scripts/Translators/PerceptionTranslator.py
+++ scripts/Translators/PerceptionTranslator.py
@@ -4,7 +4,7 @@ import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Float64
 from std_msgs.msg import Float32
-from nav_msgs.Odometry import Message
+from nav_msgs.msg import Odometry
 from threading import Semaphore   
 
 # @author: Patrick Gavigan
@@ -107,7 +107,7 @@ def rosMain():
     rospy.Subscriber('sensors/Beacon', String, translateBeacon, (perceptionPublisher))
     rospy.Subscriber('battery/charge_ratio', Float32, translateBattery, (perceptionPublisher))
     rospy.Subscriber('sensors/Bumper', String, translateBumper, (perceptionPublisher))
-    rospy.Subscriber('odom', Message, translateOdometer, (perceptionPublisher))
+    rospy.Subscriber('odom', Odometry, translateOdometer, (perceptionPublisher))
     rospy.spin()
 
 if __name__ == '__main__':
