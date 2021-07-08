6d3ef8a954c8674b0d177d65b31d2ccb6dc2c324
Patrick Gavigan <pgavigan@gmail.com>
2021-07-08 14:44:09 -0400

    converting orientation to angles

--- scripts/Translators/PerceptionTranslator.py
+++ scripts/Translators/PerceptionTranslator.py
@@ -5,7 +5,8 @@ from std_msgs.msg import String
 from std_msgs.msg import Float64
 from std_msgs.msg import Float32
 from nav_msgs.msg import Odometry
-from threading import Semaphore   
+from threading import Semaphore 
+import math 
 
 # @author: Patrick Gavigan
 # @author: Simon Yacoub
@@ -18,12 +19,41 @@ batteryPerception = ""
 irPerception = ""
 beaconPerception = ""
 bumperPerception = ""
+odomPositionPerception = ""
+odomOrientationPerception = ""
 updateReady = [False,False,False,False]
 batteryIndex = 0
 irIndex = 1
 beaconIndex = 2
 bumperIndex = 3
+odomPositionIndex = 4
+odomOrientationIndex = 5
 sem = Semaphore()
+
+
+
+# https://automaticaddison.com/how-to-convert-a-quaternion-into-euler-angles-in-python/
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+     
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+     
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+     
+    return roll_x, pitch_y, yaw_z # in radians
 
 # Translates sensor info from battery/charge_ratio into perceptions
 def translateBattery(data, args):
@@ -81,8 +111,11 @@ def translateBumper(data, args):
 def translateOdometer(data, args):
     position = (data.pose.pose.position.x, data.pose.pose.position.y, data.pose.pose.position.z)
     orientation = (data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
+    (x, y, z, w) = orientation
+    angles = euler_from_quaternion(x, y, z, w)
+    
     print(position)
-    print(orientation)
+    print(angles)
     
 
 def sendUpdate(publisher):
