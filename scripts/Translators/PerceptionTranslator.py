8de7690a6269524b45559ba2999324c5334fc5c1
Patrick Gavigan <pgavigan@gmail.com>
2021-07-09 10:03:48 -0400

    commented unused perceptions out of the translator

--- scripts/Translators/PerceptionTranslator.py
+++ scripts/Translators/PerceptionTranslator.py
@@ -2,7 +2,7 @@
 
 import rospy
 from std_msgs.msg import String
-from std_msgs.msg import Float64
+#from std_msgs.msg import Float64
 from std_msgs.msg import Float32
 from nav_msgs.msg import Odometry
 from threading import Semaphore 
@@ -16,18 +16,18 @@ import math
 # "battery(...) irWall(.....) beacon(.....) bumper(.....)"
 
 batteryPerception = ""
-irPerception = ""
-beaconPerception = ""
+#irPerception = ""
+#beaconPerception = ""
 bumperPerception = ""
 odomPositionPerception = ""
 odomOrientationPerception = ""
-updateReady = [False,False,False,False,False,False]
+updateReady = [False,False,False,False]#,False,False]
 batteryIndex = 0
 bumperIndex = 1
 odomPositionIndex = 2
 odomOrientationIndex = 3
-irIndex = 4
-beaconIndex = 5
+#irIndex = 4
+#beaconIndex = 5
 sem = Semaphore()
 
 
@@ -67,32 +67,32 @@ def translateBattery(data, args):
     sendUpdate(perceptionPublisher)
 
 # Translates sensor info from /sensor/Infrared into perceptions
-def translateIR(data, args):
-    (perceptionPublisher) = args
-    ir = data.data
-    global irPerception, updateReady, irIndex, sem
-    sem.acquire()
-    irPerception = "irWall({})".format(ir)
-    updateReady[irIndex] = True
-    sem.release()
-    sendUpdate(perceptionPublisher)
+# def translateIR(data, args):
+#     (perceptionPublisher) = args
+#     ir = data.data
+#     global irPerception, updateReady, irIndex, sem
+#     sem.acquire()
+#     irPerception = "irWall({})".format(ir)
+#     updateReady[irIndex] = True
+#     sem.release()
+#     sendUpdate(perceptionPublisher)
 
 # Translates sensor info from /sensor/Beacon into perceptions
-def translateBeacon(data, args):
-    (perceptionPublisher) = args
-    beacon = data.data
-    #rospy.loginfo("Raw data from beacon: " + beacon)
-    #beacon = beacon.split("$")
-    global beaconPerception, updateReady, beaconIndex, sem
-    sem.acquire()
-    beaconPerception = beacon.replace(":","")
-    #for aBeacon in beacon:
-    #    if(aBeacon != ''):
-    #        aBeacon = aBeacon.replace(":","")
-    #        beaconPerception = " beacon(" + aBeacon + ") " + beaconPerception
-    updateReady[beaconIndex] = True
-    sem.release()
-    sendUpdate(perceptionPublisher)
+# def translateBeacon(data, args):
+#     (perceptionPublisher) = args
+#     beacon = data.data
+#     #rospy.loginfo("Raw data from beacon: " + beacon)
+#     #beacon = beacon.split("$")
+#     global beaconPerception, updateReady, beaconIndex, sem
+#     sem.acquire()
+#     beaconPerception = beacon.replace(":","")
+#     #for aBeacon in beacon:
+#     #    if(aBeacon != ''):
+#     #        aBeacon = aBeacon.replace(":","")
+#     #        beaconPerception = " beacon(" + aBeacon + ") " + beaconPerception
+#     updateReady[beaconIndex] = True
+#     sem.release()
+#     sendUpdate(perceptionPublisher)
 
     # Translates sensor info from /sensor/Bumper into perceptions
 def translateBumper(data, args):
@@ -128,7 +128,8 @@ def sendUpdate(publisher):
     global batteryPerception, odomPositionPerception, odomOrientationPerception, beaconPerception, bumperPerception, updateReady, sem
     sem.acquire()    
     if not False in updateReady:
-        perception = batteryPerception + " " + irPerception + " " + beaconPerception + " " + bumperPerception  + " " + odomPositionPerception + " " + odomOrientationPerception
+        # perception = batteryPerception + " " + irPerception + " " + beaconPerception + " " + bumperPerception  + " " + odomPositionPerception + " " + odomOrientationPerception
+        perception = batteryPerception + " " + bumperPerception  + " " + odomPositionPerception + " " + odomOrientationPerception
         rospy.loginfo(perception)
         publisher.publish(perception)
         for i in range(len(updateReady)):
@@ -147,8 +148,8 @@ def sendAsyncUpdate(publisher, message):
 def rosMain():
     rospy.init_node('PerceptionTranslator', anonymous=True)
     perceptionPublisher = rospy.Publisher('perceptions', String, queue_size=10)
-    rospy.Subscriber('sensors/Infrared', String, translateIR, (perceptionPublisher))
-    rospy.Subscriber('sensors/Beacon', String, translateBeacon, (perceptionPublisher))
+    #rospy.Subscriber('sensors/Infrared', String, translateIR, (perceptionPublisher))
+    #rospy.Subscriber('sensors/Beacon', String, translateBeacon, (perceptionPublisher))
     rospy.Subscriber('battery/charge_ratio', Float32, translateBattery, (perceptionPublisher))
     rospy.Subscriber('sensors/Bumper', String, translateBumper, (perceptionPublisher))
     rospy.Subscriber('odom', Odometry, translateOdometer, (perceptionPublisher))
